c9633d0ef44ead022a147ffff6af1839ca6a62f9
JohnMichael <jmokvf@mail.missouri.edu>
2018-08-23 12:42:13 -0500

    Initial Commit

---
+++ My First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My First App
+//
+//  Created by Jack Oatey on 8/23/18.
+//  Copyright © 2018 John Oatey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
